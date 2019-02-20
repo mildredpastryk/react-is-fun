f68b835e16684b87a628695b0d5dbc6dffefcc37
Mildred Pastryk <mildred.pastryk@gmail.com>
2019-02-20 11:40:37 +0100

    add style and logo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+import logo from './logo.svg';
+import './App.css';
 
 let skiData = {
 	total: 50,
@@ -16,24 +18,19 @@ const calcGoalProgress = (total, goal) => {
 }
 
 const SkiDayCounter = ({total, powder, backcountry, goal}) => {
-    return (
-       <section>
-		  <div>
-			<p>Total Days: {total}</p>
-		  </div>
-		  <div>
-			<p>Powder Days: {powder}</p>
-		  </div>
-		  <div>
-			<p>Backcountry Days: {backcountry}</p>
-		  </div>
-		  <div>
-			<p>Goal Progress: {calcGoalProgress(total, goal)}</p>
-		  </div>
-        </section>
-    )
+	return (
+		<section className = "App-header">
+            <div><h1 className = "app-title">Ski Day Counter</h1></div>
+			<div>
+                <p>Total Days: {total}</p>
+			    <p>Powder Days: {powder}</p>
+			    <p>Backcountry Days: {backcountry}</p>
+			    <p>Goal Progress: {calcGoalProgress(total, goal)}</p>
+            </div>
+            <span><img src={logo} className="App-logo" alt="logo" /></span>
+		</section>
+	)
 }
-
 
 render(
 	<SkiDayCounter 
@@ -41,5 +38,6 @@ render(
 		powder={skiData.powder}
 		backcountry={skiData.backcountry}
 		goal={skiData.goal}/>, 
+    
 	document.getElementById('root')
 )
