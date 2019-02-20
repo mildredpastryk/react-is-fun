e755f03fbb9edb6fa2be1ed1a6b1853c02c09ceb
Mildred Pastryk <mildred.pastryk@gmail.com>
2019-02-20 09:49:50 +0100

    full refactor of component as function to access props

--- src/index.js
+++ src/index.js
@@ -8,39 +8,32 @@ let skiData = {
 	goal: 100
 }
 
-const SkiDayCounter = (props) => {
+const getPercent = decimal => {
+	return decimal * 100 + '%'
+}
+const calcGoalProgress = (total, goal) => {
+	return getPercent(total/goal)
+}
+
+const SkiDayCounter = ({total, powder, backcountry, goal}) => {
     return (
        <section>
 		  <div>
-			<p>Total Days: {props.total}</p>
+			<p>Total Days: {total}</p>
 		  </div>
 		  <div>
-			<p>Powder Days: {props.powder}</p>
+			<p>Powder Days: {powder}</p>
 		  </div>
 		  <div>
-			<p>Backcountry Days: {props.backcountry}</p>
+			<p>Backcountry Days: {backcountry}</p>
 		  </div>
 		  <div>
-			<p>Goal Progress: {props.goal)}</p>
+			<p>Goal Progress: {calcGoalProgress(total, goal)}</p>
 		  </div>
         </section>
     )
 }
 
-// class SkiDayCounter extends Component {
-// getPercent = decimal => {
-//		return decimal * 100 + '%'
-//	}
-//	calcGoalProgress = (total, goal) => {
-//		return this.getPercent(total/goal)
-//	}
-//	render() {
-//	   const {total, powder, backcountry, goal} = this.props
-//	   return (
-//				
-//		)
-//	}
-//}
 
 render(
 	<SkiDayCounter 
